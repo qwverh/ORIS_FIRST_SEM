9bd42afbf550b993c461b6a156ad5e9790b5e02a
artem_qwverh <artem.kuzmin281@gmail.com>
2026-01-16 11:47:54 +0300

    the first sem work

--- first_sem/src/main/java/controller/WelcomeServlet.java
+++ first_sem/src/main/java/controller/WelcomeServlet.java
@@ -6,14 +6,17 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-@WebServlet("/welcome")
+@WebServlet(urlPatterns = {"/", "/welcome"})
 public class WelcomeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.setAttribute("currentContext", req.getContextPath());
         req.getRequestDispatcher("/start.ftlh").forward(req, resp);
     }
 }
